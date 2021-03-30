d2308bc5e51454777e683f3ff8def1b4ec9ac053
Tri Truong <truongductri01@gmail.com>
2021-03-30 16:14:49 -0500

    question 3.3

--- Node_Express/exercises/index.js
+++ Node_Express/exercises/index.js
@@ -25,6 +25,16 @@ app.get("/info", (request, response) => {
         `
   );
 });
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  console.log(id);
+  const person = phoneBook.find((person) => person.id == id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
 // Give the app a port to listen to
 const PORT = 3001;

81a6512f9d50223a88f5a4ad4cac050d44807abf
Tri Truong <truongductri01@gmail.com>
2021-03-30 09:26:35 -0500

    Set up simple web server and return a simple hello world onto the webpage screen

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// Simple Web server
+
+const http = require("http");
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World");
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log("Server Running on port", PORT);
